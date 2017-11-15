8e7286d65293044303c5b77e2daaabd4ba818609
shaurya96 <jain.shaurya96@gmail.com>
2017-11-16 01:30:56 +0530

    Second Commit

--- app/src/main/java/com/example/hp/cabpool_app/LoginActivity.java
+++ app/src/main/java/com/example/hp/cabpool_app/LoginActivity.java
@@ -1,41 +1,226 @@
 package com.example.hp.cabpool_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     Button login_btn;
     TextView new_user;
+    EditText user_Email_field;
+    EditText user_Password_field;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login_btn =(Button)findViewById(R.id.btnLogin);
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
+        SharedPreferences sharedPreferences = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
 
-                // Start NewActivity.class
+        boolean hasLoggedIn = sharedPreferences.getBoolean("hasLoggedIn", false);
+
+           if(hasLoggedIn)
+           {
+               Intent myIntent = new Intent(LoginActivity.this,
+                       DashboardActivity.class);
+               startActivity(myIntent);
+           }
+
+            login_btn = (Button) findViewById(R.id.btnLogin);
+
+            user_Email_field = (EditText) findViewById(R.id.userEmailField);
+            user_Password_field = (EditText) findViewById(R.id.userPasswordField);
+
+
+            login_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0)
+            {
+                saveInfo(login_btn);
+                RetrieveData();
                 Intent myIntent = new Intent(LoginActivity.this,
                         DashboardActivity.class);
                 startActivity(myIntent);
             }
         });
-        new_user = (TextView)findViewById(R.id.link_to_register);
+        new_user = (TextView) findViewById(R.id.link_to_register);
         new_user.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-
-                // Start NewActivity.class
                 Intent myIntent = new Intent(LoginActivity.this,
-                       SignUpActivity.class);
+                        SignUpActivity.class);
                 startActivity(myIntent);
             }
         });
+
     }
+
+    //save user data
+    public void saveInfo(Button btn)
+    {
+        if (btn == login_btn)
+        {
+            SharedPreferences sharedPreferences = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+             editor.putBoolean("hasLoggedIn", true);
+             editor.apply();
+        }
+    }
+    //retrieve user data and convert it to jsonObject
+    public void RetrieveData()
+   {
+
+          String email = user_Email_field.getText().toString();
+          String password = user_Password_field.getText().toString();
+
+          JSONObject jsonObject = new JSONObject();
+       try
+       {
+           jsonObject.put("username",email);
+           jsonObject.put("password",password);
+       }
+       catch (JSONException e)
+       {
+           e.printStackTrace();
+       }
+       //Log.d(TAG, String.valueOf(jsonObject));
+       new sendLoginJsonDataToServer().execute(jsonObject);
+   }
+}
+
+    class sendLoginJsonDataToServer extends AsyncTask<Object,Void,String>
+  {
+      private static final String LOG_TAG = "loginactivity";
+      private static final String LOGIN_REQUEST_URL =
+              "http://13.127.36.190:3000/users/login";
+    @Override
+    protected String doInBackground(Object... objects)
+    {
+        //json data to be posted on server
+        JSONObject jsonObject = (JSONObject) objects[0];
+        String jsonlogindata =  jsonObject.toString();
+        String jsonResponse = "";
+        InputStream inputStream = null;
+        HttpURLConnection conn = null;
+        Log.d(LOG_TAG,jsonlogindata);
+        //url to call
+
+        URL url = createUrl(LOGIN_REQUEST_URL);
+        try
+        {
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+
+            // post jsondata to server
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type","application/json");
+
+            OutputStream out = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(out, "UTF-8"));
+
+            writer.write(jsonlogindata);
+            writer.flush();
+            writer.close();
+            out.close();
+            conn.connect();
+
+            if(conn.getResponseCode()==200)
+            {
+                inputStream = conn.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            }
+            else {
+                Log.e(TAG, "Error response code: " + conn.getResponseCode());
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if(conn!= null)
+            {
+                conn.disconnect();
+            }
+            if (inputStream != null)
+            {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return jsonResponse;
+    }
+
+      @Override
+      protected void onPostExecute(String s)
+      {
+          if(s==null)
+          {
+              return;
+          }
+          else
+          {
+              Log.d(TAG,s);
+          }
+      }
+
+      private String readFromStream(InputStream inputStream) throws IOException {
+          StringBuilder output = new StringBuilder();
+          if (inputStream != null) {
+              InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+              BufferedReader reader = new BufferedReader(inputStreamReader);
+              String line = reader.readLine();
+              while (line != null) {
+                  output.append(line);
+                  line = reader.readLine();
+              }
+          }
+          return output.toString();
+      }
+
+      @Nullable
+        private URL createUrl(String stringUrl) {
+         URL url  ;
+          try {
+              url = new URL(stringUrl);
+          } catch (MalformedURLException e) {
+              e.printStackTrace();
+              return null;
+          }
+          return url;
+      }
 }
--- app/src/main/java/com/example/hp/cabpool_app/SignUpActivity.java
+++ app/src/main/java/com/example/hp/cabpool_app/SignUpActivity.java
@@ -1,25 +1,70 @@
 package com.example.hp.cabpool_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class SignUpActivity extends AppCompatActivity
+{
 
     Button sign_up;
     TextView linkTologin;
+    EditText full_name;
+    EditText Mobile_no;
+    EditText Email;
+    EditText Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        full_name = (EditText)findViewById(R.id.reg_fullname);
+        Mobile_no = (EditText)findViewById(R.id.reg_phoneNo);
+        Email = (EditText)findViewById(R.id.reg_email);
+        Password = (EditText) findViewById(R.id.reg_password);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+
+        boolean hasLoggedIn = sharedPreferences.getBoolean("hasSignedIn", false);
+        if(hasLoggedIn)
+        {
+            Intent myIntent = new Intent(SignUpActivity.this,
+                    DashboardActivity.class);
+            startActivity(myIntent);
+        }
+
+
+
         sign_up = (Button)findViewById(R.id.btnRegister);
         sign_up.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
-                // Start NewActivity.class
+                saveInfo(sign_up);
+                RetrieveData();
                 Intent myIntent = new Intent(SignUpActivity.this,
                         DashboardActivity.class);
                 startActivity(myIntent);
@@ -35,5 +80,147 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
+
+
+
+    private void saveInfo(Button btn)
+    {
+        if (btn == sign_up)
+        {
+            SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("hasSignedIn", true);
+            editor.apply();
+        }
+    }
+
+    private void RetrieveData()
+    {
+        String name = full_name.getText().toString();
+        String mobile = Mobile_no.getText().toString();
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+
+        JSONObject jsonObject = new JSONObject();
+        try
+        {
+            jsonObject.put("name",name);
+            jsonObject.put("username",email);
+            jsonObject.put("password",password);
+            jsonObject.put("phone",mobile);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        new sendSignupJsonDataToServer().execute(jsonObject);
+
+    }
+}
+class sendSignupJsonDataToServer extends AsyncTask<Object,Void,String>
+{
+    private static final String TAG = "signupactivity";
+    private static final String SIGN_UP_REQUEST_URL =
+            "http://13.127.36.190:3000/users/signup";
+
+    @Override
+    protected String doInBackground(Object... objects)
+    {
+
+        JSONObject jsonObject = (JSONObject) objects[0];
+        String jsonsignupdata =  jsonObject.toString();
+        String jsonResponse = "";
+        InputStream inputStream = null;
+        HttpURLConnection conn = null;
+        Log.d(TAG, jsonsignupdata);
+
+        URL url = createUrl(SIGN_UP_REQUEST_URL);
+
+        try {
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type","application/json");
+
+            OutputStream out = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(out, "UTF-8"));
+            writer.write(jsonsignupdata);
+            writer.flush();
+            writer.close();
+            out.close();
+            conn.connect();
+            if(conn.getResponseCode()==200)
+            {
+                inputStream = conn.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            }
+            else {
+                Log.e(TAG, "Error response code: " + conn.getResponseCode());
+            }
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if(conn!= null)
+            {
+              conn.disconnect();
+            }
+            if (inputStream != null)
+            {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return jsonResponse;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if(s==null)
+        {
+            return;
+        }
+        else
+        {
+            Log.d(TAG,s);
+        }
+    }
+
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    @Nullable
+    private URL createUrl(String stringUrl)
+    {
+        URL url  ;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return url;
     }
 }
