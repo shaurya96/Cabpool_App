18453486b845c677fe6f057594ef81ff16f8f1dd
shaurya96 <jain.shaurya96@gmail.com>
2017-11-22 18:56:53 +0530

    Fifth commit

--- app/src/main/java/com/example/hp/cabpool_app/PoolRequestActivity.java
+++ app/src/main/java/com/example/hp/cabpool_app/PoolRequestActivity.java
@@ -2,7 +2,6 @@ package com.example.hp.cabpool_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class PoolRequestActivity extends AppCompatActivity {
     @Override
@@ -10,11 +9,6 @@ public class PoolRequestActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poolreq);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar1);
-        myToolbar.setTitleTextColor(0xFFFFFFFF);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        myToolbar.setTitle("Requests");
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new PoolRequestFragment())
                 .commit();
